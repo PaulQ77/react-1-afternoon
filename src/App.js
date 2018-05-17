f6f5adec4cd660a151767b6795e2364ceb870ba0
Paul Quiroz <pquiroz1977@gmail.com>
2018-05-16 17:16:27 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import './index.css';
+import TopicBrowser from './components/TopicBrowser/TopicBrowser';
+import EvenAndOdd from './components/Topics/EvenAndOdd';
 
 class App extends Component {
   render() {
-    
+    return (
+      <TopicBrowser/>
+      
+    )
   }
 }
 
